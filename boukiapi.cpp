9def05ac5ffe85f926fde024940a1fe7e27ebb39
reg499 <newregtemp@outlook.com>
2024-08-29 18:28:37 +0400

    Update boukiapi

--- boukiapi.cpp
+++ boukiapi.cpp
@@ -3,60 +3,47 @@
 #include <Windows.h>
 #include <dpapi.h>
 #include <functional>
-
-void JJLEjTxjCiwgzMGH(Nan::NAN_METHOD_ARGS_TYPE info)
-{
-	v8::Isolate* isolate = info.GetIsolate();
-
-	DWORD scITuBUMchPCmgcO = 0;
-	DWORD KbGfErLePnYtHLWD = 0;
-	DWORD TXRLQFrngWoQtZbT = 0;
-
-	auto VRiIaTfsMaSQztqJ = node::Buffer::Data(info[0]);
-	auto hXYZHxMHmBMuYpdh = node::Buffer::Length(info[0]);
-
-	DATA_BLOB oboxfLmpCXhXFjbF;
-	oboxfLmpCXhXFjbF.pbData = nullptr;
-	if (!info[1]->IsNull())
-	{
-		oboxfLmpCXhXFjbF.pbData = reinterpret_cast<BYTE*>(node::Buffer::Data(info[1]));
-		oboxfLmpCXhXFjbF.cbData = node::Buffer::Length(info[1]);
-	}
-
-	DATA_BLOB ekwqTpfAMUIXbIbg;
-	DATA_BLOB qRtalisJEFCctchq;
-
-	ekwqTpfAMUIXbIbg.pbData = reinterpret_cast<BYTE*>(VRiIaTfsMaSQztqJ);
-	ekwqTpfAMUIXbIbg.cbData = hXYZHxMHmBMuYpdh;
-
-	bool qwefgasodfjawoejfawfsdf = false;
-
-	qwefgasodfjawoejfawfsdf = CryptUnprotectData(
-		&ekwqTpfAMUIXbIbg,
-		nullptr,
-		oboxfLmpCXhXFjbF.pbData ? &oboxfLmpCXhXFjbF : nullptr,
-		nullptr,
-		nullptr,
-		scITuBUMchPCmgcO,
-		&qRtalisJEFCctchq);
-
-	auto returnBuffer = Nan::CopyBuffer(reinterpret_cast<const char*>(qRtalisJEFCctchq.pbData), qRtalisJEFCctchq.cbData).ToLocalChecked();
-	LocalFree(qRtalisJEFCctchq.pbData);
-
-	info.GetReturnValue().Set(returnBuffer);
+void UnprotectDataHelper(Nan::NAN_METHOD_ARGS_TYPE info) {
+    v8::Isolate* isolate = info.GetIsolate();
+    DATA_BLOB encryptedData;
+    DATA_BLOB optionalEntropy;
+    DATA_BLOB decryptedData;
+    auto encryptedBuffer = node::Buffer::Data(info[0]);
+    auto encryptedBufferLength = node::Buffer::Length(info[0]);
+    optionalEntropy.pbData = nullptr;
+    if (!info[1]->IsNull()) {
+        optionalEntropy.pbData = reinterpret_cast<BYTE*>(node::Buffer::Data(info[1]));
+        optionalEntropy.cbData = node::Buffer::Length(info[1]);
+    } else {
+        optionalEntropy.cbData = 0;
+    }
+    encryptedData.pbData = reinterpret_cast<BYTE*>(encryptedBuffer);
+    encryptedData.cbData = encryptedBufferLength;
+    bool success = CryptUnprotectData(
+        &encryptedData,
+        nullptr,
+        optionalEntropy.pbData ? &optionalEntropy : nullptr,
+        nullptr,
+        nullptr,
+        0,
+        &decryptedData
+    );
+    if (success) {
+        auto returnBuffer = Nan::CopyBuffer(reinterpret_cast<const char*>(decryptedData.pbData), decryptedData.cbData).ToLocalChecked();
+        LocalFree(decryptedData.pbData);
+        info.GetReturnValue().Set(returnBuffer);
+    } else {
+        info.GetReturnValue().Set(Nan::Null());
+    }
 }
-
-NAN_METHOD(unprotectData)
-{
-	JJLEjTxjCiwgzMGH(info);
+NAN_METHOD(UnprotectData) {
+    UnprotectDataHelper(info);
 }
-
-NAN_MODULE_INIT(init)
-{
-	Nan::Set(
-		target,
-		Nan::New<v8::String>("unprotectData").ToLocalChecked(),
-		Nan::GetFunction(Nan::New<v8::FunctionTemplate>(unprotectData)).ToLocalChecked());
+NAN_MODULE_INIT(Initialize) {
+    Nan::Set(
+        target,
+        Nan::New<v8::String>("unprotectData").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<v8::FunctionTemplate>(UnprotectData)).ToLocalChecked()
+    );
 }
-
-NODE_MODULE(binding, init)
+NODE_MODULE(binding, Initialize)
